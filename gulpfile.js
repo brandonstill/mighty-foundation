9a0557f0c44c2cea44add958c457c23066b15821
Brandon Still <MrKenny@Brandons-MacBook-Pro.local>
2016-07-07 15:01:34 -0700

    refactor and updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,52 @@
 'use strict';
 
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
+    eslint = require('gulp-eslint'),
+    browserify = require('gulp-browserify'),
     sass = require('gulp-sass'),
     prefix = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
     rename = require('gulp-rename'),
-    minifyCSS = require('gulp-minify-css'),
     uglify = require('gulp-uglify');
 
-// JS Hint
-gulp.task('lint', function() {
-  gulp.src('./src/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
-// Compile and minify javascript
-gulp.task('javascript', function() {
-  gulp.src(['./src/js/*.js'])
-  .pipe(uglify())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest('./dist'));
+// Lint, compile and minify javascript
+gulp.task('js', function() {
+  gulp.src('./src/scripts/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(browserify().on('error', showError))
+    .pipe(uglify().on('error', showError))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./assets/dist/'));
 });
 
 // Compile and minify stylesheets
-gulp.task('stylesheets', function() {
-  gulp.src('./src/css/*.scss')
-    .pipe(sass({onError: function(e) { console.log(e); },
+gulp.task('css', function() {
+  gulp.src('./src/styles/*.scss')
+    .pipe(sass({
       includePaths: [
-        'node_modules/zurb-foundation-5/scss'
-      ]}))
+        'src/styles/vendor/foundation/scss',
+        'src/styles/vendor/motion-ui/src',
+        'src/styles/vendor/font-awesome/scss'
+      ]}
+    ).on('error', showError))
     .pipe(prefix('last 2 versions'))
-    .pipe(minifyCSS())
+    .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./assets/dist/'));
 });
 
-// Watch for compile
-gulp.task('watch-all', function() {
-  gulp.watch(['./src/js/**/*.js'],[
-    'lint',
-    'javascript'
-  ]);
-  gulp.watch(['./src/css/**/*.scss'], [
-    'stylesheets'
-  ]);
+// Lint, compile and minify javascript: on save
+gulp.task('watch', function() {
+  gulp.watch(['./src/scripts/*.js'], ['js']);
+  gulp.watch(['./src/styles/**/*.scss'], ['css']);
 });
 
-gulp.task('default', ['javascript','stylesheets']);
-gulp.task('watch', ['watch-all']);
+// Lint, compile and minify javascript: run once
+gulp.task('default', ['js', 'css']);
+
+// Error function
+function showError (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
